9e0b553f19350b6753806052a18b740bba2f89c7
Diagne <cheikh.diagne2@etu.univ-lorraine.fr>
2023-05-05 17:34:37 +0200

    first commit

---
+++ myapp/lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MyApp(),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // <== définit le thème Light
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        // <== définit le thème Dark
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark, // <== on choisit le Dark
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Première appli'),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.3),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 12,
+          ),
+          child: Row(
+            children: [
+              const Text('Hello World'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
